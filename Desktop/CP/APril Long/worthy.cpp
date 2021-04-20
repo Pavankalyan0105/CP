14798ea7d2d955693a53bc3fcd8b88fe807c09ee
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-04-20 20:33:38 +0530

    ...

--- Desktop/CP/APril Long/worthy.cpp
+++ Desktop/CP/APril Long/worthy.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 using namespace std;
+typedef long long ll;
 
 void solve(){
-    int r , c, k;
+    ll r , c, k;
     cin>>r>>c>>k;
 
-    long long arr[1000000][1000000] =  {0};
+    long long arr[r+1][c+1] =  {0};
 
-    for(int i=1;i<=r;i++)
-        for(int j=1;j<=c;j++)
+    for(long long i=1;i<=r;i++)
+        for(long long j=1;j<=c;j++)
             cin>>arr[i][j];
 
-    long long p[100000][1000000];
-    for(int i=1;i<=r;i++)
-        for(int j=1;j<=c;j++)
+    long long p[r+1][c+1];
+    for(long long i=1;i<=r;i++)
+        for(long long j=1;j<=c;j++)
             p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1] + arr[i][j];
 
     int count = 0;
-    int n = (r<c)?r:c;
-    int sm=0;
+    long long n = (r<c)?r:c;
+    long long sm=0;
 
-    for(int i=1;i<=n;i++)
-        for(int x=1;x<=(r-i+1);x++)
-            for(int y=1;y<=(c-i+1);y++)
+    for(ll i=1;i<=n;i++)
+        for(ll x=1;x<=(r-i+1);x++)
+            for(ll y=1;y<=(c-i+1);y++)
             {
                 sm = p[x+i-1][y+i-1] - p[x+i-1][y-1] - p[x-1][y+i-1] + p[x-1][y-1];
                 if((sm/(i*i))>=k)
